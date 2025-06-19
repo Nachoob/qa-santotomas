88b8b004d305300dd39e5aaa5c36df10f1473db1
Ignacio <ignacio.balbontin@radar.cl>
2025-06-18 22:30:10 -0400

    cambio footer seccion

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -48,5 +48,7 @@
         </div>
         <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        @yield('scripts')
+        @yield('footer')
     </body>
 </html>
