447cde2341080b465f0fc2c0bb43399fd4e40dc9
Ignacio <ignacio.balbontin@radar.cl>
2025-06-19 12:10:27 -0400

    bootstrap

---
+++ resources/lang/es/pagination.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+    'showing' => 'Mostrando',
+    'to' => 'a',
+    'of' => 'de',
+    'results' => 'resultados',
+    'go_to_page' => 'Ir a la página :page',
+    'pagination_navigation' => 'Navegación de paginación',
+];
