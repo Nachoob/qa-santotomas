39912a1e8df451244b7bf7d1df18a690cad4dc92
Nachoob <ignaciobalbontin4@gmail.com>
2025-06-23 14:37:07 -0400

    error log

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,11 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'guest' => \Illuminate\Auth\Middleware\RedirectIfAuthenticated::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'test' => \App\Http\Middleware\TestMiddleware::class,
     ];
